639363df5b38714ac1711b913c5b44f67dd93d2e
daniel loureiro <loureiro.daniel0@gmail.com>
2017-06-05 13:08:48 +0200

    rails stupid coach

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,7 +1,28 @@
 class CoachingController < ApplicationController
+
   def answer
+    @answer = params[:query]
+    @coach_answer = coach_answer(@answer)
   end
 
   def ask
+  end
+
+end
+
+
+private
+
+def coach_answer(your_message)
+  # TODO: return coach answer to your_message
+  #check if sentence = i am going to work right now
+  #check if sentence = I met a girl last night
+  #check if sentence = Can I eat some pizza?
+  if your_message == "I am going to work right now!"
+    return ""
+  elsif your_message.end_with?("?")
+    return "Silly question , get dressed and go work"
+  else
+    return "i dont care go work"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'coaching/answer'
-
-  get 'coaching/ask'
-
+  get "/answer", to: "coaching#answer"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/ask", to: "coaching#ask"
 end
