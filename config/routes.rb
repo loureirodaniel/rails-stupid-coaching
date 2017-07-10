3c83491886b461f554b8058d0d80d668ee320bfa
daniel loureiro <loureiro.daniel0@gmail.com>
2017-07-10 23:53:20 +0200

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/answer", to: "coaching#answer"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/ask", to: "coaching#ask"
+
+  root 'coaching#ask'
 end
